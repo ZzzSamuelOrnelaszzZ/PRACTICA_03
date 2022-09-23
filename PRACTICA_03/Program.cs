6b9291957643ceabc1c37c56ada9d4751a24c5cf
itsdo <itsdo@DESKTOP-42QB0C4>
2022-09-23 12:47:35 -0700

    Agregar archivos de proyecto.

---
+++ PRACTICA_03/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACTICA_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numlados;
+            float medidalado, perimetro;
+
+            Console.WriteLine("ingresa la cantidad de lados de la figura: ");
+            numlados = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese medida de uno de los lados: ");
+            medidalado= Single.Parse(Console.ReadLine());
+
+            perimetro = numlados * medidalado;
+
+            Console.WriteLine("el perimetro es: {0}", perimetro);
+
+        }
+    }
+}
